f6353c2fac960697c8923159840137ca1d630ddb
Tarek Mostafa <tarek.mostafa@zalando.de>
2021-09-03 15:53:57 +0200

    Add frontend skeleton

---
+++ src/metascore_assets/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { render } from "react-dom";
+import "react-toastify/dist/ReactToastify.css";
+
+document.title = "Meta Score";
+
+const App = () => {
+  return <div>Hello world</div>;
+};
+
+render(<App />, document.getElementById("root"));
