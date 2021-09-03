300b7450ba980ec04c17261779561ad661464df1
Quint Daenen <me@di-wu.be>
2021-09-03 10:58:34 +0200

    Fix score component ranking.

--- example.sh
+++ example.sh
@@ -13,6 +13,8 @@ dfx canister call metascore register "(principal \"$gameID\")"
 dfx canister call metascore getOverallRanking "(principal \"$gameID\")"
 dfx canister call metascore getRanking "(principal \"$gameID\", $playerS)"
 dfx canister call metascore getPercentile "(principal \"$gameID\", $playerS)"
+dfx canister call metascore getGameScoreComponent "(principal \"$gameID\", $playerP)"
+dfx canister call metascore getGameScoreComponent "(principal \"$gameID\", $playerS)"
 
 dfx canister call metascore cron > /dev/null
 
@@ -26,6 +28,8 @@ dfx stop
 # (vec { principal "2ibo7-dia"; principal "uuc56-gyb" })
 # (opt (2 : nat))
 # (opt (0.5 : float64))
+# (opt (1_000_000_000_000 : nat))
+# (opt (750_000_000_000 : nat))
 #
 # [Canister rrkah-fqaaa-aaaaa-aaaaq-cai] Getting scores...
 # [Canister rwlgt-iiaaa-aaaaa-aaaaa-cai] Returning scores...
