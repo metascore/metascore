6aa4bfe3849ed69ad48b7bb008243932179bf715
Jorgen <jorgen@buildnode.io>
2021-09-14 17:03:14 -0700

    fix tests

--- example.sh
+++ example.sh
@@ -46,7 +46,14 @@ check "Register invalid game" "$(dfx canister call metascore register "(principa
 
 check "Register game" "$(dfx canister call game register "(principal \"$metascoreID\")")" "(variant { ok })"
 
-check "Get games" "$(dfx canister call metascore getGames)" "(vec { record { name = \"Saga Tarot\" } })"
+check "Get games" "$(dfx canister call metascore getGames)" "(
+  vec {
+    record {
+      principal "ryjl3-tyaaa-aaaaa-aaaba-cai";
+      record { name = "Saga Tarot" };
+    };
+  },
+)"
 
 echo ""
 
